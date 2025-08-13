9acdaf7bdf5dfb899f3e5cc2093c7f87f7d4778f
rudra93-op <ujjwaltiwari14072@gmail.com>
2025-08-13 10:56:26 +0530

    n

--- auth-system/server/server.js
+++ auth-system/server/server.js
@@ -33,14 +33,14 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: 'https://liger0p.vercel.app', //'https://aaroh-frontend.vercel.app',
+    origin: 'https://aaroh-ai.vercel.app', //'https://aaroh-frontend.vercel.app',
     methods: ["GET", "POST"],
   },
 });
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://liger0p.vercel.app", // ✅ frontend domain
+  origin: "https://aaroh-ai.vercel.app", // ✅ frontend domain
   credentials: true                             // ✅ must be true if using withCredentials
 }));
 // Routes
